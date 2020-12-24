6d56da40a6dfd3a3d020eb3d5fa1e40b6a53823f
dianamishanich <diana-mishanich@ya.ru>
2020-12-25 01:38:27 +0300

    WebDriverTest

---
+++ src/main/java/io/github/dianamishanich/ApiConfig.java
@@ -0,0 +1,24 @@
+package io.github.dianamishanich;
+
+import org.aeonbits.owner.Config;
+
+import java.net.URL;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "classpath:api.properties",
+        "file:../secret.properties",
+        "system:properties"
+})
+public interface ApiConfig extends Config {
+    @Config.DefaultValue("https://yandex.ru")
+    @Key("base.url")
+    URL baseUrl();
+
+    @Key("secret.token")
+    String tokenSecret();
+
+    @Key("non.secret.token")
+    String tokenNonSecret();
+
+}
---
+++ src/main/java/io/github/dianamishanich/WebDriverProvider.java
@@ -0,0 +1,59 @@
+package io.github.dianamishanich;
+
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.function.Supplier;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverProvider implements  Supplier<WebDriver> {
+
+    @Override
+    public WebDriver get() {
+
+        final WebDriverConfig config = ConfigFactory.newInstance().create(WebDriverConfig.class);
+
+        if (config.remote()) {
+
+            String browserType;
+
+            switch (config.browserName()) {
+                case CHROME:
+                    browserType = BrowserType.CHROME;
+                    break;
+                case OPERA:
+                    browserType = BrowserType.OPERA;
+                    break;
+                default:
+                    throw new RuntimeException("Unknown browser name: " + config.browserName());
+            }
+
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities(browserType, "", Platform.ANY);
+            desiredCapabilities.setCapability("enableVNC", true);
+            desiredCapabilities.setCapability("enableVideo", true);
+
+            return new RemoteWebDriver(config.remoteURL(), desiredCapabilities);
+
+        } else {
+
+            switch (config.browserName()) {
+                case CHROME:
+                    WebDriverManager.chromedriver().setup();
+                    return new ChromeDriver();
+                case OPERA:
+                    WebDriverManager.operadriver().setup();
+                    return new OperaDriver();
+                default:
+                    throw new RuntimeException("Unknown browser name: " + config.browserName());
+            }
+
+        }
+    }
+}
